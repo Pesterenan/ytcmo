390f4ef60ee910a8bef20e566a31070b85de4e7f
Pesterenan <pesterenan@gmail.com>
2023-02-15 15:54:25 -0300

    [YTCMO]: Added method to replace timecode

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 import fs from "fs";
-import { sortByName, filterSuper, formatName } from "./utils.js";
+import {
+  sortByName,
+  filterSuper,
+  formatName,
+  formatTimecodeToYoutubeTimeCode,
+  MPC_TIMECODE_REGEX
+} from "./utils.js";
+import { ChildProcess } from "child_process";
 
 const CSV_FILE = fs.readFileSync("input.csv", "utf-8");
-console.log("Welcome to YTCMO\n\n");
+const filterLevel = "Super Pestinha!";
+console.log("Welcome to YTCMO");
 
-const dataObj = CSV_FILE.split("\n");
-dataObj.pop();
-dataObj.shift();
+const formatYoutubeMemberships = (inputFile, level) => {
+  const dataObj = inputFile.split("\n");
+  dataObj.pop();
+  dataObj.shift();
 
-let memberships = {};
-memberships = dataObj
-  .map((member) => {
-    const memberFormatted = member.split(",");
-    return {
-      name: memberFormatted[0],
-      currentLevel: memberFormatted[2],
-    };
-  })
-  .sort(sortByName)
-  .filter(filterSuper);
+  let memberships = {};
+  memberships = dataObj
+    .map((member) => {
+      const memberFormatted = member.split(",");
+      return {
+        name: memberFormatted[0],
+        currentLevel: memberFormatted[2],
+      };
+    })
+    .sort(sortByName)
+    .filter((member) => filterSuper(member, level));
 
-const members = memberships.reduce((acc, member) => {
-  acc += formatName(member.name);
-  acc += "\n";
-  return acc;
-}, "");
+  const members = memberships.reduce((acc, member) => {
+    acc += formatName(member.name, level);
+    acc += "\n";
+    return acc;
+  }, "");
 
-console.log(members);
+  console.log(members);
+};
+const timecode = `
+00:00:01.123                   : First chapter
+00:01:00.345                   : Second chapter
+`
+const result = formatTimecodeToYoutubeTimeCode(timecode,MPC_TIMECODE_REGEX);
+console.log(result);
--- utils.js
+++ utils.js
@@ -5,8 +5,8 @@ export const sortByName = (a, b) => {
   return 0;
 };
 
-export const filterSuper = (member) =>
-  member.currentLevel === "Super Pestinha!";
+export const filterSuper = (member, level) =>
+  member.currentLevel === level;
 
 export const formatName = (memberName) => {
   let name = memberName.split(" ");
@@ -17,3 +17,15 @@ export const formatName = (memberName) => {
 };
 
 const capitalizeFirstLetter = (word) => word.toLowerCase().replace(/^\w/, (c) => c.toUpperCase());
+
+export const MPC_TIMECODE_REGEX = /\.\d{3}\W+\:\W/gm;
+/**
+ * This method replaces the characters from the seconds to the
+ * label name of each chapter in a video by the string ' - '
+ * @param {string} inputTimecode 
+ * @param {string} regex 
+ * @returns 
+ */
+export const formatTimecodeToYoutubeTimeCode = (inputTimecode, regex) => {
+  return inputTimecode.replaceAll(regex, ' - ');
+}
